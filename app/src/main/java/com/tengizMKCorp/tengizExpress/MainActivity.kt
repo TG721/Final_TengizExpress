5d3df60fddf0b84a533f9c6a1224f901feb40209
Tengiz <Tengiz152@gmail.com>
2022-11-04 04:25:13 +0400

    working on Search fragment, part 2, bottom Navigation works

--- app/src/main/java/com/tengizMKCorp/tengizExpress/MainActivity.kt
+++ app/src/main/java/com/tengizMKCorp/tengizExpress/MainActivity.kt
@@ -2,13 +2,28 @@ package com.tengizMKCorp.tengizExpress
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.replace
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.tengizMKCorp.tengizExpress.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val navController = binding.fragmentContainerView.getFragment<NavHostFragment>().navController
+        setContentView(binding.root)
+
+        val bottomNavigation = binding.bottomNavigationView
+        bottomNavigation.setupWithNavController(navController)
     }
 
 }
